b376e237827b06da63abfc74bd7d3de55edf3990
deadpool205 <iwilljustdiy@gmail.com>
2020-08-02 03:04:08 -0400

    app skeleton and routes set

---
+++ src/pages/Home.js
@@ -0,0 +1,47 @@
+/* eslint jsx-a11y/anchor-is-valid: 0 */
+
+import React from 'react'
+import ServiceItem from  'components/service/ServiceItem'
+import { getServices } from 'store'
+import Hero from 'components/Hero'
+
+class Home extends React.Component {
+
+  state = {
+    services: []
+  }
+
+  componentDidMount() {
+    const services = getServices()
+    this.setState({services: services})
+  }
+
+  renderServices = (services) => 
+  services.map(service => <ServiceItem key={service.id} service={service} />)
+
+  render() {
+    const { services } = this.state
+    return (
+      <div>
+        <Hero />
+        <section className="section section-feature-grey is-medium">
+          <div className="container">
+            <div className="title-wrapper has-text-centered">
+              <h2 className="title is-2">Great Power Comes </h2>
+              <h3 className="subtitle is-5 is-muted">With great Responsability</h3>
+              <div className="divider is-centered"></div>
+            </div>
+
+            <div className="content-wrapper">
+              <div className="columns">
+              { this.renderServices(services) }
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Home
